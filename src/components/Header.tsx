dd26d6a46f6ce665f1f23f3dc24043ec859d25d6
raphpay <raphpay.dev@gmail.com>
2025-05-14 14:37:53 +0400

    Feature: Update header style #7

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,30 +7,35 @@ const Header = () => {
 
   return (
     <header className="flex justify-between p-4 bg-muted">
-      <h1 className="text-3xl font-bold text-primary tracking-tight">
-        Clubbix
-      </h1>
-      {user ? (
-        <div className="flex items-center gap-4">
-          <span className="text-sm">{user.email}</span>
-          <LogoutButton />
+      <div className="flex flex-col gap-2 w-full">
+        <div className="flex justify-between">
+          <h1 className="text-3xl font-bold text-primary tracking-tight">
+            Clubbix
+          </h1>
+          {user ? (
+            <div className="flex items-center gap-4">
+              <span className="text-sm">{user.email}</span>
+              <LogoutButton />
+            </div>
+          ) : (
+            <div className="flex gap-2">
+              <Link
+                to="/signup"
+                className="text-primary hover:underline font-medium"
+              >
+                Créer un compte
+              </Link>
+              <Link
+                to="/login"
+                className="text-primary hover:underline font-medium"
+              >
+                Connexion
+              </Link>
+            </div>
+          )}
         </div>
-      ) : (
-        <div className="flex gap-2">
-          <Link
-            to="/signup"
-            className="text-primary hover:underline font-medium"
-          >
-            Créer un compte
-          </Link>
-          <Link
-            to="/login"
-            className="text-primary hover:underline font-medium"
-          >
-            Connexion
-          </Link>
-        </div>
-      )}
+        <div className="w-full h-[1px] bg-black rounded-full" />
+      </div>
     </header>
   );
 };
