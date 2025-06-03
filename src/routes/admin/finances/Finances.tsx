c13a8ee6ce820a96e52a1aac0d10a978d6df8973
raphpay <raphpay.dev@gmail.com>
2025-06-03 16:11:49 +0400

    Fix: Correct title #12

--- src/routes/admin/finances/Finances.tsx
+++ src/routes/admin/finances/Finances.tsx
@@ -167,7 +167,7 @@ const Finances = () => {
   return (
     <div className="max-w-6xl mx-auto p-6">
       <div className="flex justify-between items-center mb-6">
-        <h1 className="text-2xl font-bold text-secondary">Liste des membres</h1>
+        <h1 className="text-2xl font-bold text-secondary">Finances</h1>
         <div className="flex gap-2">
           <ButtonPrimary
             title={"+ Ajouter une entrée"}
