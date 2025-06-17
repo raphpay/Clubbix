a276f356a6775835ca6c952679bc33046c685503
raphpay <raphpay.dev@gmail.com>
2025-06-17 18:03:19 +0400

    Feature: Handle auth persistance #4

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactNode, useState } from "react";
+import React, { createContext, ReactNode, useEffect, useState } from "react";
 import { User } from "../services/auth";
 
 interface AuthContextType {
@@ -21,12 +21,22 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({
   const login = (user: User) => {
     setIsAuthenticated(true);
     setUser(user);
+    localStorage.setItem("user", JSON.stringify(user));
   };
 
   const logout = () => {
     setIsAuthenticated(false);
     setUser(null);
+    localStorage.removeItem("user");
   };
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+      setIsAuthenticated(true);
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
