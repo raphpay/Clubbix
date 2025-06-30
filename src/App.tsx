64331f461d36ac90bae3a3743eecc78fb756eb9b
raphpay <raphpay.dev@gmail.com>
2025-06-30 14:48:27 +0400

    Feature: Animate loading spinner #49

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ const AppContent: React.FC = () => {
   if (loading) {
     return (
       <div className="flex h-screen items-center justify-center">
-        <p>Loading...</p>
+        <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-blue-600 border-t-transparent" />
       </div>
     );
   }
